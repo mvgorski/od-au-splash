182598809c963cb2cf999405b2951edebc103946
mvgorski <matthewvgorski@gmail.com>
2016-12-15 14:29:36 -0800

    clean up, js to new file

--- js/script-2017.js
+++ js/script-2017.js
@@ -5,13 +5,16 @@ Matt Gorski
 
 This needs:
 
-1.
-2.
-3.
+1. SVGs for logos / PNGs for fallback logos
+2. Including SVGeezy script for SVG > PNG fallback.
 
-The first parameter is a class to tell the code not to check. Feel free to pass in false if you want SVGeezy to check all images. This may be because you have no fallbacks for certain SVGs.
+svgeezy.init(false, 'png');
 
-The second is a filetype, this can be anything you want, just make sure the file path resolves to an image. ie. '/images/logo.svg', will be replaced with '/images/logo.png'.
+The first parameter is a class to tell the code not to check. 
+Feel free to pass in 'false' if you want SVGeezy to check all images. 
+
+The second is a filetype, this can be anything you want, 
+just make sure the file path resolves to an image. ie. '/images/logo.svg', will be replaced with '/images/logo.png'.
 
 
 */
