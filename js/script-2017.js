a6df86768082061f52d006ca43cd3ce46098a935
mvgorski <matthewvgorski@gmail.com>
2016-12-15 14:00:56 -0800

    svg add

--- js/script-2017.js
+++ js/script-2017.js
@@ -9,10 +9,37 @@ This needs:
 2.
 3.
 
+The first parameter is a class to tell the code not to check. Feel free to pass in false if you want SVGeezy to check all images. This may be because you have no fallbacks for certain SVGs.
+
+The second is a filetype, this can be anything you want, just make sure the file path resolves to an image. ie. '/images/logo.svg', will be replaced with '/images/logo.png'.
+
+
 */
 
 $(document).ready(function() {
 
+//initialize SVGeezy
+
+svgeezy.init(false, 'png'); 
+
+// hide sponsor images initially besides generic
+  $(".sponsor-br, .sponsor-ml, .copyright-br, .copyright-ml").hide();
+
+  $("#brisbane").mouseover(function(){
+    $(".sponsor-br").fadeIn();
+    $(".sponsor-default").hide();
+    $(".sponsor-ml").hide();
+    $(".copyright-default").hide();
+    $(".copyright-ml").hide();
+  });
+  $("#melbourne").mouseover(function(){
+    $(".sponsor-ml").fadeIn();
+    $(".sponsor-default").hide();
+    $(".sponsor-br").hide();
+    $(".copyright-default").hide();
+    $(".copyright-br").hide();
+
+  });
 
 
 
@@ -31,5 +58,4 @@ $(document).ready(function() {
 
 
 
-
-});
+}); // ready
